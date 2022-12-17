e15de1e22f68d675953d4354d59742a4317875a9
sambtran <albertolucascb@gmail.com>
2022-12-17 20:32:34 +0100

    v1.2

--- js/ej9.js
+++ js/ej9.js
@@ -32,6 +32,33 @@ buscar.addEventListener("input", function (evento) {
     }
   }, 100);
 });
+/*Evitamos con esta expresion regular la inserción de espacios en blanco*/
+let reg = /[^\s-]/
+function anadir(anadir){
+  {
+    console.log(valoraux);
+    if ((valoraux !== undefined)&&(reg.test(valoraux))) {
+      let elementonuevo = document.createElement("p");
+      let divnuev = document.createElement("div");
+      divnuev.className = "listado";
+      let subdiv = creardivcheck();
+      divnuev.appendChild(subdiv);
+      divnuev.appendChild(elementonuevo);
+      let insertarantesque = document.getElementsByClassName("listado");
+      let insertar = insertarantesque[0];
+      elementonuevo.innerHTML = valoraux;
+      console.log(divnuev.innerHTML);
+      let traslación = insertarantesque.length;
+      eliminar(divnuev)
+
+      lista.appendChild(divnuev);
+      ContadorDePalabrasGuardadas++;
+      localStorage.setItem(ContadorDePalabrasGuardadas, valoraux);
+      centinela = true;
+      checkear()
+    }
+  }
+}
 function creardivcheck(){
     let subdiv = document.createElement("div")
     subdiv.setAttribute("class", "checkeo");
@@ -50,32 +77,13 @@ for (let i = 1; i < 101; i++) {
     let subdiv = creardivcheck();
     divnuev.appendChild(subdiv);
     divnuev.appendChild(elementonuevo);
+    eliminar(divnuev)
     lista.appendChild(divnuev);
     ContadorDePalabrasGuardadas = i;
+    checkear()
   }
 }
-boton.addEventListener("click", function (anadir) {
-  anadir.preventDefault();
-  if (valoraux !== undefined) {
-    let elementonuevo = document.createElement("p");
-    let divnuev = document.createElement("div");
-    divnuev.className = "listado";
-    let subdiv = creardivcheck();
-    
-    divnuev.appendChild(subdiv);
-    divnuev.appendChild(elementonuevo);
-    let insertarantesque = document.getElementsByClassName("listado");
-    let insertar = insertarantesque[0];
-    elementonuevo.innerHTML = valoraux;
-    console.log(divnuev.innerHTML);
-    let traslación = insertarantesque.length;
-    lista.appendChild(elementonuevo);
-    ContadorDePalabrasGuardadas++;
-    localStorage.setItem(ContadorDePalabrasGuardadas, valoraux);
-    centinela = true;
-    reloadStylesheets()
-  }
-});
+boton.addEventListener("click",anadir);
 /* Descomentar esto y refrescar para resetear la lista guardada  */
 let reset = document.getElementById("reset")
 reset.addEventListener("click", function(reset){
@@ -83,3 +91,141 @@ reset.addEventListener("click", function(reset){
     document.location.reload(true)
 
 })
+document.addEventListener("keypress", function(entrar){
+  if (entrar.key == "Enter") {
+    anadir()
+  }
+})
+/*Codeamos el boton check*/
+function checkear(){
+function verprimero(){
+  let lista =document.getElementById("lista")
+  return lista.firstChild
+}
+function check(check){
+  
+}
+/*Elemento para crear los criculos de chekeo *conocido varios bugs* */
+let checks = document.getElementsByClassName("checkeo")
+for (let check of checks) {
+  check.addEventListener("click", function checkeando (evento){
+    console.log(check);
+    let elementonuevo = document.createElement("p");
+    elementonuevo.innerHTML = check.parentNode.childNodes[1].textContent ;
+    console.log("texto elemento"+check.parentNode.childNodes[1].textContent);
+    let divnuev = document.createElement("div");
+    divnuev.className = "listado";
+    let subdiv = creardivcheck();
+    subdiv.className="clickado"
+    divnuev.appendChild(subdiv);
+    subdiv.innerHTML="<div class='letrita'></div>"
+    divnuev.appendChild(elementonuevo);
+    for (let i = 1; i < 101; i++) {
+      if (localStorage.getItem(i)==(check.parentNode.childNodes[1].textContent)) {
+
+      }
+    }
+    subdiv.addEventListener("contextmenu",function checkneg(checkneg){
+      checkneg.preventDefault()
+      subdiv.className="checkneg"
+      subdiv.innerHTML="<p>X</p>"
+    })
+    
+    check.parentNode.parentNode.removeChild(check.parentNode);
+    eliminar(divnuev)
+    lista.insertBefore(divnuev,verprimero())
+    
+  });
+  check.addEventListener("contextmenu",function checkneg(checkneg){
+    checkneg.preventDefault()
+    check.className="checkneg"
+    check.innerHTML="<p>X</p>"
+  })
+}}
+/*boton de eliminar*/
+function eliminar(div){
+  let eliminar = document.createElement("div")
+  eliminar.className = "eliminar"
+  eliminar.innerHTML="<p>Eliminar</p>"
+  div.appendChild(eliminar)
+} 
+/* Contador para que tarde un segundo en eliminar la tarea y se pueda reporducir la animación */
+setInterval(() => {
+  let eliminars = document.getElementsByClassName("eliminar")
+  for (let eliminar of eliminars) {
+    eliminar.addEventListener("click",function(ev){
+      eliminar.parentElement.style.transform="scale(0)"
+      eliminar.parentElement.style.transition=" all 1s ease-in-out"
+      let contador = 1
+      console.log("textoewdfefdeaf  "+eliminar.parentElement.innerHTML);
+      setInterval(() => {
+        if (contador == 0) {
+          eliminar.parentElement.parentElement.removeChild(eliminar.parentElement)  
+          contador=-1
+        }
+        }, 1000);
+        if(contador>-1){
+        contador--}
+
+    })
+  }
+}, 100);
+/*Funcion para añadir boton de eliminar */
+function eliminar(div){
+  let eliminar = document.createElement("div")
+  eliminar.className = "eliminar"
+  eliminar.innerHTML="<p>Eliminar</p>"
+  div.appendChild(eliminar)
+} 
+/*Intento de arreglar bug conocido no hacer mucho caso */
+setInterval(() => {
+  let eliminars = document.getElementsByClassName("checkneg")
+  console.log(eliminars.length);
+
+  for (let eliminar of eliminars) {
+    function verprimero(){
+      let lista =document.getElementById("lista")
+      return lista.firstChild
+    }
+    function check(check){
+      
+    }
+    let checks = document.getElementsByClassName("checkneg")
+    for (let check of checks) {
+      check.addEventListener("click", function checkeando (evento){
+        console.log("CLICKADO");
+        let elementonuevo = document.createElement("p");
+        elementonuevo.innerHTML = check.parentNode.childNodes[1].textContent ;
+        console.log("texto elemento"+check.parentNode.childNodes[1].textContent);
+        let divnuev = document.createElement("div");
+        divnuev.className = "listado";
+        let subdiv = creardivcheck();
+        subdiv.className="clickado"
+        divnuev.appendChild(subdiv);
+        subdiv.innerHTML="<div class='letrita'></div>"
+        divnuev.appendChild(elementonuevo);
+        for (let i = 1; i < 101; i++) {
+          if (localStorage.getItem(i)==(check.parentNode.childNodes[1].textContent)) {
+    
+          }
+        }
+        subdiv.addEventListener("contextmenu",function checkneg(checkneg){
+          checkneg.preventDefault()
+          subdiv.className="checkneg"
+          subdiv.innerHTML="<p>X</p>"
+        })
+        
+        check.parentNode.parentNode.removeChild(check.parentNode);
+        eliminar(divnuev)
+        lista.insertBefore(divnuev,verprimero())
+        
+      });
+      check.addEventListener("contextmenu",function checkneg(checkneg){
+        checkneg.preventDefault()
+        check.className="checkneg"
+        check.innerHTML="<p>X</p>"
+      })
+    }
+  }
+  
+}, 500);
